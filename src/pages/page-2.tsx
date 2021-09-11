2f1c59c2b82a83a789d38f2dafcc8bdd141a4ffa
Shiwei Wang <38160380+AirWSW@users.noreply.github.com>
2021-09-11 19:16:08 +0800

    fix build error

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import { PageProps, Link } from 'gatsby';
 
-import Layout from "../components/Layout"
-import Seo from "../components/seo"
+import Layout from '../components/Layout';
+import Seo from '../components/seo';
 
-const SecondPage = () => (
-  <Layout location={null}>
+const SecondPage: React.FC<PageProps> = ({ location }) => (
+  <Layout location={location}>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
